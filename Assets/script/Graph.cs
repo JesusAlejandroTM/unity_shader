1058abbf1fe42b066009bc67f5b827041f44eb5d
distinguised gentleman <133259204+ClearlyHere@users.noreply.github.com>
2024-09-26 21:33:28 +0200

    added perfomance indicators, switching functions and interpolation when functions are switched

--- Assets/script/Graph.cs
+++ Assets/script/Graph.cs
@@ -7,7 +7,18 @@ public class Graph : MonoBehaviour
     [SerializeField] Transform pointPrefab;
     [SerializeField, Range(10, 100)] private int resolution;
     [SerializeField] FunctionLibrary.FunctionName function;
+    [SerializeField, Min(0f)] private float functionDuration = 1f, transitionDuration = 1f; 
+    
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField] private TransitionMode transitionMode;
+    
     private Transform[] _points;
+
+    float _duration;
+    bool _transitioning;
+    
+    FunctionLibrary.FunctionName _transitionFunction;
     
 
     void Awake()
@@ -26,6 +37,35 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _duration += Time.deltaTime;
+        if (_transitioning)
+        {
+            if (_duration >= transitionDuration)
+            {
+                _duration -= transitionDuration;
+                _transitioning = false;
+            }
+        }
+        else if (_duration >= functionDuration)
+        {
+            _duration -= functionDuration;
+            _transitioning = true;
+            _transitionFunction = function;
+            PickNextFunction();
+        }
+
+        if (_transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+
+    void UpdateFunction()
+    {
         FunctionLibrary.Function libFunction = FunctionLibrary.GetFunction(function);
         float time = Time.time; 
         float step = 2f / resolution;
@@ -42,5 +82,35 @@ public class Graph : MonoBehaviour
             float u = (x + 0.5f) * step - 1f;
             _points[i].localPosition = libFunction(u, v, time);
         }
+    }
+    
+    void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(_transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = _duration / transitionDuration;
+        float time = Time.time; 
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < _points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z++;
+                v = (z + 0.5f) * step - 1f;
+            }
+
+            float u = (x + 0.5f) * step - 1f;
+            _points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
+        }
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle
+            ? FunctionLibrary.GetNextFunctionName(function)
+            : FunctionLibrary.GetRandomFunctionOtherThan(function);
     }
 }
